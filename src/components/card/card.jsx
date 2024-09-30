20c046f44ab160d826c3a3a266c278664775a22e
Thailan Gouvea <thailangm@icloud.com>
2024-09-30 10:27:02 +0300

    Sets up home body basic layout with mocking data

---
+++ src/components/card/card.jsx
@@ -0,0 +1,9 @@
+import styles from "./card.module.css";
+
+export default function Card() {
+  return (
+    <div className={styles.cardContainer}>
+      <img src="https://rukminim2.flixcart.com/image/850/1000/poster/x/p/6/posterskart-breaking-bad-cover-poster-pkbb19-medium-original-imaebf5hqjvbcmmh.jpeg?q=90&crop=false" />
+    </div>
+  );
+}
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,40 +1,102 @@
 import Header from "../../components/header/header";
+import Card from "../../components/card/card";
 import styles from "./home.module.css";
+import { useEffect, useState } from "react";
+
+const imagesList = [];
+
+function multiplyImages() {
+  for (let i = 0; i < 10; i++) {
+    imagesList.push(<Card key={i} />);
+  }
+}
 
 export default function Home() {
+  multiplyImages();
+
   return (
     <div className={styles.homeContainer}>
       <Header />
       <div className={styles.bodyContainer}>
-        <div className={styles.listContainer}>
+        <div className={styles.categoryContainer}>
           <h2>Categoria</h2>
+          <ul className={styles.listContainer}>
+            {imagesList.map((item, index) => {
+              return <li key={index}>{item}</li>;
+            })}
+          </ul>
         </div>
-        <div className={styles.listContainer}>
+        <div className={styles.categoryContainer}>
           <h2>Categoria</h2>
+          <ul className={styles.listContainer}>
+            {imagesList.map((item, index) => {
+              return <li key={index}>{item}</li>;
+            })}
+          </ul>
         </div>
-        <div className={styles.listContainer}>
+        <div className={styles.categoryContainer}>
           <h2>Categoria</h2>
+          <ul className={styles.listContainer}>
+            {imagesList.map((item, index) => {
+              return <li key={index}>{item}</li>;
+            })}
+          </ul>
         </div>
-        <div className={styles.listContainer}>
+        <div className={styles.categoryContainer}>
           <h2>Categoria</h2>
+          <ul className={styles.listContainer}>
+            {imagesList.map((item, index) => {
+              return <li key={index}>{item}</li>;
+            })}
+          </ul>
         </div>
-        <div className={styles.listContainer}>
+        <div className={styles.categoryContainer}>
           <h2>Categoria</h2>
+          <ul className={styles.listContainer}>
+            {imagesList.map((item, index) => {
+              return <li key={index}>{item}</li>;
+            })}
+          </ul>
         </div>
-        <div className={styles.listContainer}>
+        <div className={styles.categoryContainer}>
           <h2>Categoria</h2>
+          <ul className={styles.listContainer}>
+            {imagesList.map((item, index) => {
+              return <li key={index}>{item}</li>;
+            })}
+          </ul>
         </div>
-        <div className={styles.listContainer}>
+        <div className={styles.categoryContainer}>
           <h2>Categoria</h2>
+          <ul className={styles.listContainer}>
+            {imagesList.map((item, index) => {
+              return <li key={index}>{item}</li>;
+            })}
+          </ul>
         </div>
-        <div className={styles.listContainer}>
+        <div className={styles.categoryContainer}>
           <h2>Categoria</h2>
+          <ul className={styles.listContainer}>
+            {imagesList.map((item, index) => {
+              return <li key={index}>{item}</li>;
+            })}
+          </ul>
         </div>
-        <div className={styles.listContainer}>
+        <div className={styles.categoryContainer}>
           <h2>Categoria</h2>
+          <ul className={styles.listContainer}>
+            {imagesList.map((item, index) => {
+              return <li key={index}>{item}</li>;
+            })}
+          </ul>
         </div>
-        <div className={styles.listContainer}>
+        <div className={styles.categoryContainer}>
           <h2>Categoria</h2>
+          <ul className={styles.listContainer}>
+            {imagesList.map((item, index) => {
+              return <li key={index}>{item}</li>;
+            })}
+          </ul>
         </div>
       </div>
     </div>
