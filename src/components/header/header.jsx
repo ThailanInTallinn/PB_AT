3bea77eb14daf8337c9938d38de34c9417d11af2
Thailan Gouvea <thailangm@icloud.com>
2024-09-30 11:54:25 +0300

    Makes the home page responsive

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,12 +1,27 @@
 import { Link } from "react-router-dom";
 import styles from "./header.module.css";
+import { useState } from "react";
 
 export default function Header() {
+  const [menuToggle, setMenuToggle] = useState(false);
   return (
     <div className={styles.headerContainer}>
       <Link to={"/"} className={styles.headerLogo}>
         <h1>ArleteFlix</h1>
       </Link>
+      <span
+        onClick={() => {
+          setMenuToggle(!menuToggle);
+        }}
+      >
+        =
+      </span>
+      <nav style={{ display: menuToggle ? "flex" : "" }}>
+        <div className={styles.mobileOptions}>
+          <input type="text" placeholder="Buscar" />
+          <button>Toggle theme</button>
+        </div>
+      </nav>
       <input type="text" placeholder="Buscar" />
       <button>Toggle theme</button>
     </div>
