9cd2ebbc027de2ad56543459de179a4bbc555815
Thailan Gouvea <thailangm@icloud.com>
2024-12-12 17:28:36 +0200

    Fixes signin bug

--- src/authentication.js
+++ src/authentication.js
@@ -2,6 +2,16 @@ import { redirect } from "react-router-dom";
 import axios from "axios";
 import { renderIntoDocument } from "react-dom/test-utils";
 
+const options = {
+  method: "GET",
+  url: "https://api.themoviedb.org/3/authentication/token/new",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzYzljMTVkZjY4YTc4OWM2YWFhMmE4MzliOWZjMDJjZCIsIm5iZiI6MTcyNDE1MDQ4My42MTQsInN1YiI6IjY2YzQ3MmQzZTk2NjFkMzNmZDk2YTMwNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.FeoBm65lFmd2XuDMDnToY73wD4UJ5MvACBjRFYVt4T0",
+  },
+};
+
 async function isAuthenticated() {
   const sessionToken = localStorage.getItem("sessionID");
 
