b5ca0d1e10284e07cbaa8efbb8d1fa56d1da4267
Thailan Gouvea <thailangm@icloud.com>
2024-12-12 19:47:05 +0200

    Adds more categories on movies page.

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -29,6 +29,26 @@ const popularMovies = {
   },
 };
 
+const upcoming = {
+  method: "GET",
+  url: "https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzYzljMTVkZjY4YTc4OWM2YWFhMmE4MzliOWZjMDJjZCIsIm5iZiI6MTcyOTAxMDMxNi4xMTk0MTUsInN1YiI6IjY2YzQ3MmQzZTk2NjFkMzNmZDk2YTMwNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ksUKOcK-sBVNzC3lR54wAzewqEpyheexkftNYlxB9og",
+  },
+};
+
+const topRated = {
+  method: "GET",
+  url: "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzYzljMTVkZjY4YTc4OWM2YWFhMmE4MzliOWZjMDJjZCIsIm5iZiI6MTcyOTAxMDMxNi4xMTk0MTUsInN1YiI6IjY2YzQ3MmQzZTk2NjFkMzNmZDk2YTMwNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ksUKOcK-sBVNzC3lR54wAzewqEpyheexkftNYlxB9og",
+  },
+};
+
 function multiplyImages() {
   for (let i = 0; i < 10; i++) {
     imagesList.push(<Card key={i} id={i} />);
@@ -37,6 +57,8 @@ function multiplyImages() {
 
 export default function Home() {
   const [popularMoviesList, setPopularMoviesList] = useState([]);
+  const [upcomingList, setUpcomingList] = useState([]);
+  const [topRatedList, setTopRatedList] = useState([]);
   const { category, setCategory } = useAppContext();
 
   async function getPopularMovies() {
@@ -45,9 +67,23 @@ export default function Home() {
     });
   }
 
+  async function getUpcoming() {
+    await axios.request(upcoming).then(function (response) {
+      setUpcomingList(response.data.results);
+    });
+  }
+
+  async function getTopRated() {
+    await axios.request(topRated).then(function (response) {
+      setTopRatedList(response.data.results);
+    });
+  }
+
   useEffect(() => {
     //authentication();
     getPopularMovies();
+    getUpcoming();
+    getTopRated();
   }, []);
 
   return (
@@ -70,74 +106,32 @@ export default function Home() {
           </ul>
         </div>
         <div className={styles.categoryContainer}>
-          <h2>Categoria</h2>
+          <h2>Próximos lançamentos</h2>
           <ul className={styles.listContainer}>
-            {imagesList.map((item, index) => {
-              return <li key={index}>{item}</li>;
+            {upcomingList.map((item, index) => {
+              return (
+                <li key={index}>
+                  <Card
+                    id={item.id}
+                    movieImg={`https://image.tmdb.org/t/p/original/${item.poster_path}`}
+                  />
+                </li>
+              );
             })}
           </ul>
         </div>
         <div className={styles.categoryContainer}>
-          <h2>Categoria</h2>
+          <h2>Filmes aclamados pela crítica</h2>
           <ul className={styles.listContainer}>
-            {imagesList.map((item, index) => {
-              return <li key={index}>{item}</li>;
-            })}
-          </ul>
-        </div>
-        <div className={styles.categoryContainer}>
-          <h2>Categoria</h2>
-          <ul className={styles.listContainer}>
-            {imagesList.map((item, index) => {
-              return <li key={index}>{item}</li>;
-            })}
-          </ul>
-        </div>
-        <div className={styles.categoryContainer}>
-          <h2>Categoria</h2>
-          <ul className={styles.listContainer}>
-            {imagesList.map((item, index) => {
-              return <li key={index}>{item}</li>;
-            })}
-          </ul>
-        </div>
-        <div className={styles.categoryContainer}>
-          <h2>Categoria</h2>
-          <ul className={styles.listContainer}>
-            {imagesList.map((item, index) => {
-              return <li key={index}>{item}</li>;
-            })}
-          </ul>
-        </div>
-        <div className={styles.categoryContainer}>
-          <h2>Categoria</h2>
-          <ul className={styles.listContainer}>
-            {imagesList.map((item, index) => {
-              return <li key={index}>{item}</li>;
-            })}
-          </ul>
-        </div>
-        <div className={styles.categoryContainer}>
-          <h2>Categoria</h2>
-          <ul className={styles.listContainer}>
-            {imagesList.map((item, index) => {
-              return <li key={index}>{item}</li>;
-            })}
-          </ul>
-        </div>
-        <div className={styles.categoryContainer}>
-          <h2>Categoria</h2>
-          <ul className={styles.listContainer}>
-            {imagesList.map((item, index) => {
-              return <li key={index}>{item}</li>;
-            })}
-          </ul>
-        </div>
-        <div className={styles.categoryContainer}>
-          <h2>Categoria</h2>
-          <ul className={styles.listContainer}>
-            {imagesList.map((item, index) => {
-              return <li key={index}>{item}</li>;
+            {topRatedList.map((item, index) => {
+              return (
+                <li key={index}>
+                  <Card
+                    id={item.id}
+                    movieImg={`https://image.tmdb.org/t/p/original/${item.poster_path}`}
+                  />
+                </li>
+              );
             })}
           </ul>
         </div>
